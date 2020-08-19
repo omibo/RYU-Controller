f589f4ceea62ac7f45cc87f1c0b0a493100393ab
Mobina <shbmobina@gmail.com>
2020-08-19 18:34:45 +0000

    Update runner1.py

--- runner1.py
+++ runner1.py
@@ -5,11 +5,13 @@ from mininet.log import setLogLevel, info
 from mininet.node import RemoteController
 from mininet.link import TCLink, Intf
 from mininet.cli import CLI
+from mininet.node import OVSSwitch
 
-from threading import Thread
+from threading import Timer
 from random import randint
 
 from topo1 import Topo1
+import time
 
 
 
@@ -20,14 +22,16 @@ class Runner1:
         self.net = Mininet(topo=Topo1(), link=TCLink, switch=OVSSwitch, host=CPULimitedHost, ipBase='10.0.0.0/8')
 
     def changeBW(self):
-        threading.Timer(10.0, self.changeBW).start()
         for link in self.net.links:
             link.intf1.params['bw'] = randint(1, 5)
+        Timer(10.0, self.changeBW).start()
 
     def printBW(self):
-        threading.Timer(5.0, self.changeBW).start()
         for link in self.net.links:
-            print(link.intf1.params['bw'])
+            print(link.intf1.params['bw']),
+        print('\n')
+        Timer(5.0, self.printBW).start()
+        
 
     def run(self):
         self.net.start()
@@ -35,7 +39,8 @@ class Runner1:
         self.changeBW()
         self.printBW()
 
-        CLI(self.net)
+        # CLI(self.net)
+        time.sleep(40)
 
         self.net.stop()
 
